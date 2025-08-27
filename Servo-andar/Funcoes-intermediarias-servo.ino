c328b0c8ecfd52c4dbceb87c72ba737b0168eeb0
Isabela Sakomura <isabelasakomura@gmail.com>
2025-08-27 14:41:47 -0300

    Update Funcoes-intermediarias-servo.ino

--- Servo-andar/Funcoes-intermediarias-servo.ino
+++ Servo-andar/Funcoes-intermediarias-servo.ino
@@ -1,4 +1,21 @@
 // Arquivo com as funcoes parciais para definir o andar do robo com os Servomotores
+//Inclusao das bibliotecas 
+#include<Arduino.h>
+#include<Servo.h>
+// Declaracao dos pinos a serem colocados nos servomotores
+//     > > >                         verificar qual seria o melhor posicionamento dos pinos, se seria melhor que eles fossem proximos ou separados...
+#define pinDIR 24
+#define pinESQ 30 
+#define pinQUADRIL 32
+// Variaveis para definir a posicao do servo;
+// Valores inciais atribuidos as posicoes; 
+// > > >                             depois testar qual seria essa posição e em qual posição especificamente manter o servo parado
+
+// Declaracao dos servomotores dos pes; 
+Servo servodir;
+Servo servoesq;
+Servo servoquadril; 
+
 // Incrementos das posicoes dos Servos
 int posDIR = 0; 
 int incrementoDIR = +1; 
