948ea41862b4051f1f5b3a0f21f32e400052cf04
Isabela Sakomura <isabelasakomura@gmail.com>
2025-08-28 10:01:57 -0300

    Update Servo-andar.ino

--- Servo-andar/Servo-andar/Servo-andar.ino
+++ Servo-andar/Servo-andar/Servo-andar.ino
@@ -9,7 +9,9 @@
 #define pinDIR 24
 #define pinESQ 30 
 #define pinQUADRIL_DIR 32
-#define pinQUADRIL_ESQ 34
+#define pinQUADRIL_ESQ 34;
+#define pinQUADRIL 35;
+
 // Variaveis para definir a posicao do servo;
 // Valores inciais atribuidos as posicoes; 
 // > > > >                            depois testar qual seria essa posição e em qual posição especificamente manter o servo parado
@@ -19,10 +21,11 @@
 Servo pe_dir;
 Servo pe_esq;
 Servo quadril_dir; // VERIFICAR SE VAI USAR MESMO TRES SERVOS PARA O QUADRIL OU SE SÓ UM JÁ BASTA
-Servo quadril_esq; 
+Servo quadril_esq;
+Servo quadril; 
 
 // Variaveis de controle para o movimento do quadril 
-int pos_quadril = 90; 
+int pos_quadril = 90; // Posicao inicial do quadril
 int incremento_quadril = +1; 
 int min_quadril = 60;
 int max_quadril = 120; // Variacao de 30 graus no angulo de movimentacao do quadril
@@ -34,6 +37,7 @@ void setup() {
   pe_esq.attach(pinESQ); 
   quadril_dir.attach(pinQUADRIL_DIR); // Verificar se precisa de dois servomotores ou se daria para fazer com apenas um 
   quadril_esq.attach(pinQUADRIL_ESQ); 
+  quadril.attach(pinQUADRIL); 
   // Verificar quais posicoes dos servomotores correspondem ao 0 (Permite determinar a faixa dos angulos possiveis do servomotor)
   //    > > > > >                     depois setar essa parte para valores 0 para verificar qual o angulo que o servomotor precisa ficar parado para atingir os angulos que precisamos
   // SUGESTÃO: Talvez escrever todos os servos começando em 90° para estabelecer os valores de referencia e ele poder tanto assumir angulos negativos em relacao ao referencial quanto positivos?
@@ -44,9 +48,22 @@ void setup() {
   */
 }
 
+
+void movimento_pe(Servo servo) { // Movimento do pe do servo 
+
+
+}
+
+
+// Para dois servomotores distintos no quadril: basta colocar os incrementos opostos 
+//                > > > > >                 precisa ter um jeito de salvar dois servomotores a
 void movimento_quadril() {
+  // Codigo para um unico servomotor para o quadril 
+  if(pos_quadril<min_quadril || pos_quadril>max_quadril) incremento_quadril = - incremento_quadril;
+  pos_quadril += incremento_quadril; 
+  quadril.write(pos_quadril);
 
-
+  // Codigo para dois servomotores para o quadril 
 }
 
 // Funcoes modulares para o andar do robo
@@ -58,14 +75,12 @@ void andar_frente() {
   }
 
 
-
-
-
   //Loop: pe esquerdo avança
   while(1) {
 
     if() break; 
   }
+  delay(10); // Espera o servo atingir a posicao setada 
 
 }
 
@@ -85,27 +100,3 @@ void loop() {
   // Criar alguma rotina de interrupcao no meio do codigo
 
 }
-
-
-
-// usar a mesma logica do incremento para definir o andar do robo!! 
-int posDIR = 0; 
-int incrementoDIR = +1; 
-int posESQ = 180; 
-int incrementoESQ = +1; 
-int posQUADRIL = 0;
-int incrementoQUADRIL = +1; 
-
-void functeste() { // Funcionando! 
-    if(posDIR>180 || posDIR<0) incrementoDIR = -incrementoDIR; 
-    if(posQUADRIL>180 || posQUADRIL<0) incrementoQUADRIL = -incrementoQUADRIL; 
-    if(posESQ<0 || posESQ>180) incrementoESQ =- incrementoESQ;
-    posDIR += incrementoDIR; 
-    posESQ += incrementoESQ;
-    posQUADRIL += incrementoQUADRIL;
-    servoquadril.write(posQUADRIL); 
-    servodir.write(posDIR);
-    servoesq.write(posESQ); 
-    delay(10); // Variacao minima de angulo, logo nao precisa parar o codigo por tanto tempo 
-}
-
